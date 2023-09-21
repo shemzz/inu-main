47b361e1cded4db1392e1d981efd65b8745f78ca
bcoder <bcoder@gmail.com>
2023-09-21 18:35:19 +0100

    add error reporting

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+   ini_set('display_errors', 1);
+   
 // phpinfo();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve privkey and seedphr from POST request
